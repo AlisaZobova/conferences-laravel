12e5c223fa1f9d682a56e2d054f8886ef81d79b5
Alisa Zobova <zobovaalice@gmail.com>
2023-01-10 22:31:10 +0200

    feat: create request class and controller for conferences

---
+++ conferences/app/Http/Requests/ConferenceRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ConferenceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['string', 'required', 'min:2', 'max:255', 'regex:/[A-Z][a-z]*(\s(([A-Z][a-z]*)|([a-z]+))|(\s[0-9]+)*)*/'],
+            'conf_date' => 'required|after_or_equal:' . now(),
+            'latitude' => 'numeric|between:-90,90|nullable',
+            'longitude' => 'numeric|between:-90,90|nullable',
+        ];
+    }
+}
