018f883cbf2d6ac297bc59a83d0a8f4188ee6f1a
Alisa Zobova <zobovaalice@gmail.com>
2023-01-10 21:15:49 +0200

    feat: create migrations

--- conferences/database/migrations/2023_01_10_103847_create_countries_table.php
+++ conferences/database/migrations/2023_01_10_103847_create_countries_table.php
@@ -13,16 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('password');
-            $table->date('birthdate');
-            $table->string('phone');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -34,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('countries');
     }
 };
