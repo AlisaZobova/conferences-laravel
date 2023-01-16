a74853816b007415cfc5acb87f33e471b14587ce
Alisa Zobova <zobovaalice@gmail.com>
2023-01-16 15:19:41 +0200

    feat: add host to vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-});
+export default ({mode}) => {
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: [
+                    'resources/sass/app.scss',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+        server: {
+            host: process.env.HOST,
+        },
+    });
+}
+
