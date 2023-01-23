8f27084790ad2e82cd961366c1e3a67d3ffdb7e7
Alisa Zobova <zobovaalice@gmail.com>
2023-01-23 23:21:33 +0200

    fix: fix routes

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -5,16 +5,11 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest', 'web'] )->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::post('register/{user}', [RegisteredUserController::class, 'store_additional'])
         ->name('store_additional');
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 });
