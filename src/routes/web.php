ba5b2946335f7deffe65c932860201ac0ef8bc56
Alisa Zobova <zobovaalice@gmail.com>
2023-02-04 21:47:23 +0200

    feat: create routes for comments and reports, add new middleware to verify the author of the report

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\CountryController;
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConferenceController;
 use App\Http\Controllers\UserController;
@@ -23,27 +25,47 @@ Route::get(
     }
 );
 
-Route::get('/conferences', [ ConferenceController::class, 'index' ])->name('conferences.index');
-Route::get('/countries', [ CountryController::class, 'index' ])->name('countries.index');
+Route::get('/conferences', [ ConferenceController::class, 'index' ]);
+Route::get('/countries', [ CountryController::class, 'index' ]);
 
 Route::group(
     ['middleware' => ['permission:create conference']],
     function () {
-        Route::post('/conferences', [ ConferenceController::class, 'store' ])->name('conferences.store');
+        Route::post('/conferences', [ ConferenceController::class, 'store' ]);
     }
 );
 
 Route::middleware('auth')->group(
     function () {
         Route::get('user/{user}', [UserController::class, 'getUser']);
-        Route::get('/conferences/{conference}', [ ConferenceController::class, 'show' ])->name('conferences.show');
-        }
+        Route::get('/conferences/{conference}', [ ConferenceController::class, 'show' ]);
+        Route::get('/reports', [ ReportController::class, 'index' ]);
+        Route::get('/reports/{report}', [ ReportController::class, 'show' ]);
+        Route::get('/reports/{report}/download', [ ReportController::class, 'download' ]);
+        Route::get('reports/{report}/comments', [ CommentController::class, 'index' ]);
+        Route::get('/comments/{comment}', [ CommentController::class, 'show' ]);
+        Route::post('/comments', [ CommentController::class, 'store' ]);
+        Route::patch('/comments/{comment}', [ CommentController::class, 'update' ]);
+    }
+);
+
+Route::middleware(['auth', 'role:Announcer'])->group(
+    function () {
+        Route::post('/reports', [ ReportController::class, 'store' ]);
+    }
+);
+
+Route::middleware(['auth', 'role:Announcer', 'report_creator'])->group(
+    function () {
+        Route::patch('/reports/{report}', [ ReportController::class, 'update' ]);
+        Route::delete('/reports/{report}', [ ReportController::class, 'destroy' ]);
+    }
 );
 
 Route::middleware(['auth', 'role:Announcer|Listener'])->group(
     function () {
-        Route::post('/conferences/{conference}/join', [ UserController::class, 'join' ])->name('users.join');
-        Route::post('/conferences/{conference}/cancel', [ UserController::class, 'cancel' ])->name('users.cancel');
+        Route::post('/conferences/{conference}/join', [ UserController::class, 'join' ]);
+        Route::post('/conferences/{conference}/cancel', [ UserController::class, 'cancel' ]);
     }
 );
 
@@ -51,14 +73,14 @@ Route::middleware(['auth', 'role:Announcer|Listener'])->group(
 Route::group(
     ['middleware' => ['permission:update conference', 'creator']],
     function () {
-        Route::patch('/conferences/{conference}', [ ConferenceController::class, 'update' ])->name('conferences.update');
+        Route::patch('/conferences/{conference}', [ ConferenceController::class, 'update' ]);
     }
 );
 
 Route::group(
     ['middleware' => ['permission:delete conference', 'creator']],
     function () {
-        Route::delete('/conferences/{conference}', [ ConferenceController::class, 'destroy' ])->name('conferences.destroy');
+        Route::delete('/conferences/{conference}', [ ConferenceController::class, 'destroy' ]);
     }
 );
 
