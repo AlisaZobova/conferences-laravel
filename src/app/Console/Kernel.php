6d165b214bb4ee7defceec3bcc1c26b32484db0a
Alisa Zobova <zobovaalice@gmail.com>
2023-02-27 21:40:22 +0200

    feat: add schedule for deleting export files after 15 min

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -4,18 +4,28 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $delimeter = PHP_OS_FAMILY === 'Windows' ? '\\' : '/';
+            $files = glob(public_path('export') . $delimeter . "*.csv");
+            foreach ($files as $file) {
+                if (filemtime($file) < now()->subMinutes(15)->getTimestamp()) {
+                    unlink($file);
+                }
+            }
+        })->everyFifteenMinutes();
+
     }
 
     /**
@@ -25,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         include base_path('routes/console.php');
     }
